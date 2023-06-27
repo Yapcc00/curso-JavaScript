3b43efaaea311a5d3695ad78061c20c4c4573c06
Yeison Cordoba <yeisoncordoba672@gmail.com>
2023-06-27 15:29:58 -0500

    primera clase de programacion orientada a objetos

---
+++ POO/poo.js
@@ -0,0 +1,13 @@
+class Book{
+constructor(a,b,c){
+    
+}
+
+title = '1984';
+author = 'George Orwell';
+price = 350;
+}
+
+//Instancia  de Book
+const book1 = new Book();
+console.log(book1.author);
