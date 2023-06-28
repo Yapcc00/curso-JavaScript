59057922fd4cbcc29b3d28d247bed2ec33332eee
Yeison Cordoba <yeisoncordoba672@gmail.com>
2023-06-28 13:46:25 -0500

    Finalizacion de clase de programacio orientada a objetos

--- POO/poo.js
+++ POO/poo.js
@@ -1,5 +1,6 @@
 const _private = new WeakMap();
 
+// Clase Book (Libro)
 class Book {
     constructor(title, author, price) {
         const properties = {
@@ -11,64 +12,92 @@ class Book {
         _private.set(this, { properties });
     }
 
-    //obtiene el titulo de un libro
+    // Obtiene el título de un libro
     get title() {
         return _private.get(this).properties['_title'];
     }
 
-
-    //modified el titulo de un libro
+    // Modifica el título de un libro
     set author(newAuthor) {
         return _private.get(this).properties['_author'] = newAuthor;
     }
 
-    //obtiene el author de un libro
+    // Obtiene el autor de un libro
     get author() {
         return _private.get(this).properties['_author'];
     }
 
-
-    //modified el author de un libro
+    // Modifica el autor de un libro
     set author(newAuthor) {
         return _private.get(this).properties['_author'] = newAuthor;
     }
 
-
-    /**PRECIO  */
-
-    //obtiene el author de un libro
+    // Obtiene el precio de un libro
     get price() {
         return _private.get(this).properties['_price'];
     }
 
-
-    //modified el price de un libro
-    set price(nrePrice) {
-        return _private.get(this).properties['_price'] = nrePrice;
+    // Modifica el precio de un libro
+    set price(newPrice) {
+        return _private.get(this).properties['_price'] = newPrice;
     }
 }
 
-//se crea una clase aparte para los comics, pero se heredan algunas propiedades de book y eso se hace con 'Extends'
+// Clase Comic (Cómic) que hereda propiedades de Book (Libro)
 class Comic extends Book {
-
     constructor(name, author, price, illustrators) {
         super(name, author, price);
-        //agrega nuevas funcionalidades
+        // Agrega nuevas funcionalidades
         this.illustrators = illustrators;
     }
-    
-    addIlustrator(newIllustrator = []){
-        this.illustrators.push(newIllustrator)
-    }
 
+    addIlustrator(newIllustrator = []) {
+        this.illustrators.push(newIllustrator);
+    }
 }
 
-//Instancia  de Book
+// Clase ShoppingCart (Carrito de compras)
+class ShoppingCart {
+    constructor() {
+        this.product = [];
+    }
+
+    // Agrega productos al carrito
+    addProduct(amount, price) {
+        this.product.push(...Array(amount).fill(price));
+    }
+
+    // Muestra los productos del carrito
+    showProduct() {
+        console.log(this.product);
+    }
+
+    // Calcula el total de la compra
+    calcTotal() {
+        return this.product
+            .map(price => price)
+            .reduce((accumulator, price) => accumulator + price, 0);
+    }
+
+    // Imprime el ticket de compra
+    printTicket() {
+        console.log(`Total a pagar: ${ this.calcTotal() } `);
+    }
+}
+
+// Instancias de Book y Comic
 const book1 = new Book('1984', 'Go', 350);
+const comic1 = new Comic('The Killing Joke', 'A,M', 412, ['Y.A', 'C.P']);
 
-const comic1 = new Comic('the killing Joke', 'A,M', 412, ['Y.A', 'C.P'])
+// Instancia de ShoppingCart
+const cart = new ShoppingCart();
 
+// Agregar productos al carrito
+cart.addProduct(3, comic1.price);
+cart.addProduct(3, book1.price);
 
+// Mostrar productos en el carrito
+cart.showProduct();
 
-console.log(book1.title);
-console.log(comic1.price)
+// Imprimir ticket de compra
+cart.printTicket();
